6550a9620e6080af72e7cbb002b7439d83521f9c
Mjc0015 <mjc0015@auburn.edu>
2015-04-27 14:08:07 -0500

    Create buyOrder.java

---
+++ buyOrder.java
@@ -0,0 +1,33 @@
+package pkg.order;
+
+import pkg.exception.StockMarketExpection;
+import pkg.trader.Trader;
+
+public class BuyOrder extends Order {
+
+	public BuyOrder(String stockSymbol, int size, double price, Trader trader) {
+		this.stockSymbol = stockSymbol;
+		this.size = size;
+		this.price = price;
+		this.trader = trader;		
+	}
+
+	public BuyOrder(String stockSymbol, int size, boolean isMarketOrder,
+			Trader trader) throws StockMarketExpection {
+			if (!isMarketOrder)
+			{
+				throw new StockMarketExpection("Order has been placed without a valid price");
+			}
+			this.stockSymbol = stockSymbol;
+			this.size = size;
+			this.isMarketOrder = isMarketOrder;
+			this.price = 0.0;
+			this.trader = trader;			
+	}
+
+	public void printOrder() {
+		System.out.println("Stock: " + stockSymbol + " $" + price + " x "
+				+ size + " (Buy)");
+	}
+
+}
